e94009a45b97a67e8aa3060e391b065e3f2573b2
TreNgheDiCode <gabayan170@gmail.com>
2024-01-31 19:07:39 +0700

    Prototype Pattern

--- Client.cs
+++ Client.cs
@@ -1,4 +1,5 @@
 ﻿using Design_Pattern.AbstractFactory;
+using Design_Pattern.Prototype;
 using Design_Pattern.Singleton;
 using System.Text;
 
@@ -32,55 +33,81 @@ namespace DesignPatternsEx
         //}
         #endregion
 
-        #region Abstract Factory
+        #region Prototype
         static void Main(string[] args)
         {
-            // In ra tiếng Việt
-            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("***Prototype Pattern Demo * **\n");
 
-            Console.WriteLine("***Abstract Factory Pattern Demo***\n");
-            // Order một chú chó Hoang dã từ Đại lý
-            // Đại lý sẽ chỉ định nhà máy chịu trách nhiệm sản xuất động vật Hoang dã
-            IAnimalFactory wildAnimalFactory = new WildAnimalFactory();
+            // Khởi tạo các bản sản phẩm là Nano và Ford (đều có phụ tùng là Basic Car)
+            // Dùng Basic Car dạng như là 1 khuôn xe hơi nhưng dùng mẫu mã khác nhau để thể hiện sản phẩm khác nhau
+            // trên ModelName và Price
+            BasicCar nano_base = new Nano("Green Nano") {Price = 100000};
+            BasicCar ford_base = new Ford("Ford Yellow") {Price = 500000};
+            
+            BasicCar bc1;
 
-            // Nhà máy sử dụng khuôn để làm ra sản phẩm
-            IDog wildDog = wildAnimalFactory.CreateDog();
+            // Clone từ sản phẩm gốc (không nhầm lẫn với mẫu gốc là BasicCar nhé), ở đây là Nano
+            bc1 = nano_base.Clone();
+            bc1.Price = nano_base.Price + BasicCar.SetPrice();
+            Console.WriteLine("Car is: {0}, and it's price is Rs. {1} ",bc1.ModelName,bc1.Price);
 
-            // Nhận hàng và test sản phẩm
-            wildDog.Speak();
-            wildDog.Action();
-
-            // Sau khi xong, tiếp tục order một con hổ Hoang dã từ Đại lý
-            // Nhà máy sử dụng khuôn để làm ra sản phẩm
-            ITiger wildTiger = wildAnimalFactory.CreateTiger();
-
-            // Nhận hàng và test sản phẩm
-            wildTiger.Speak();
-            wildTiger.Action();
-
-            Console.WriteLine("******************");
-
-            // Order một chú chó Thú cưng từ Đại lý
-            // Đại lý sẽ chỉ định nhà máy chịu trách nhiệm sản xuất động vật Thú cưng
-            IAnimalFactory petAnimalFactory = new PetAnimalFactory();
-
-            // Nhà máy sử dụng khuôn để làm ra sản phẩm
-            IDog petDog = petAnimalFactory.CreateDog();
-
-            // Nhận hàng và test sản phẩm
-            petDog.Speak();
-            petDog.Action();
-
-            // Sau khi xong, tiếp tục order một con hổ Thú cưng từ Đại lý
-            // Nhà máy sử dụng khuôn để làm ra sản phẩm
-            ITiger petTiger = petAnimalFactory.CreateTiger();
-
-            // Nhận hàng và test sản phẩm
-            petTiger.Speak();
-            petTiger.Action();
-
+            // Clone từ sản phẩm gốc (không nhầm lẫn với mẫu gốc là BasicCar nhé), ở đây là Ford
+            bc1 = ford_base.Clone();
+            bc1.Price = ford_base.Price + BasicCar.SetPrice();
+            Console.WriteLine("Car is: {0}, and it's price is Rs. {1}", bc1.ModelName, bc1.Price);
             Console.ReadLine();
         }
+        #endregion
+
+        #region Abstract Factory
+        //static void Main(string[] args)
+        //{
+        //    // In ra tiếng Việt
+        //    Console.OutputEncoding = Encoding.UTF8;
+
+        //    Console.WriteLine("***Abstract Factory Pattern Demo***\n");
+        //    // Order một chú chó Hoang dã từ Đại lý
+        //    // Đại lý sẽ chỉ định nhà máy chịu trách nhiệm sản xuất động vật Hoang dã
+        //    IAnimalFactory wildAnimalFactory = new WildAnimalFactory();
+
+        //    // Nhà máy sử dụng khuôn để làm ra sản phẩm
+        //    IDog wildDog = wildAnimalFactory.CreateDog();
+
+        //    // Nhận hàng và test sản phẩm
+        //    wildDog.Speak();
+        //    wildDog.Action();
+
+        //    // Sau khi xong, tiếp tục order một con hổ Hoang dã từ Đại lý
+        //    // Nhà máy sử dụng khuôn để làm ra sản phẩm
+        //    ITiger wildTiger = wildAnimalFactory.CreateTiger();
+
+        //    // Nhận hàng và test sản phẩm
+        //    wildTiger.Speak();
+        //    wildTiger.Action();
+
+        //    Console.WriteLine("******************");
+
+        //    // Order một chú chó Thú cưng từ Đại lý
+        //    // Đại lý sẽ chỉ định nhà máy chịu trách nhiệm sản xuất động vật Thú cưng
+        //    IAnimalFactory petAnimalFactory = new PetAnimalFactory();
+
+        //    // Nhà máy sử dụng khuôn để làm ra sản phẩm
+        //    IDog petDog = petAnimalFactory.CreateDog();
+
+        //    // Nhận hàng và test sản phẩm
+        //    petDog.Speak();
+        //    petDog.Action();
+
+        //    // Sau khi xong, tiếp tục order một con hổ Thú cưng từ Đại lý
+        //    // Nhà máy sử dụng khuôn để làm ra sản phẩm
+        //    ITiger petTiger = petAnimalFactory.CreateTiger();
+
+        //    // Nhận hàng và test sản phẩm
+        //    petTiger.Speak();
+        //    petTiger.Action();
+
+        //    Console.ReadLine();
+        //}
         #endregion
 
         #region Abstract Factory - Example 2
---
+++ Prototype/DefaultPrototype.cs
@@ -0,0 +1,55 @@
+﻿namespace Design_Pattern.Prototype
+{
+    // Bản gốc (hoặc có thể coi là khuôn mẫu)
+    public abstract class BasicCar
+    {
+        public string ModelName { get; set; } = null!;
+        public int Price { get; set; } // Khi không gán gì hết, mặc định là 0
+
+        /// <summary>
+        /// Khi gọi tới sẽ trả về giá ngẫu nhiên
+        /// </summary>
+        /// <returns></returns>
+        public static int SetPrice()
+        {
+            Random r = new();
+            int p = r.Next(200000, 500000);
+            int price = p;
+            return price;
+        }
+
+        // Phương thức Clone (ràng buộc để mỗi sản phẩm kế thừa từ bản gốc này đều phải clone từ bản gốc này)
+        public abstract BasicCar Clone();
+    }
+
+    // Sản phẩm clone
+    public class Nano : BasicCar
+    {
+        public Nano(string m)
+        {
+            ModelName = m;
+        }
+
+        // Trả về clone của Nano (dù bản chất bên ngoài thể hiện là Nano, nhưng phụ tùng là của BasicCar - do tính kế thừa)
+        public override BasicCar Clone()
+        {
+            return (Nano)MemberwiseClone(); //shallow Clone
+        }
+    }
+
+    // Sản phẩm clone
+    public class Ford : BasicCar
+    {
+        public Ford(string m)
+        {
+            ModelName = m;
+        }
+
+        // Trả về clone của Ford (dù bản chất bên ngoài thể hiện là Ford, nhưng phụ tùng là của BasicCar - do tính kế thừa)
+        public override BasicCar Clone()
+        {
+            return (Ford)MemberwiseClone();
+        }
+    }
+
+}
